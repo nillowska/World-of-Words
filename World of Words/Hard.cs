8d6e43555eabeb8572fb3f65a13d2e3a25ede855
nillowska <kotlorz.paulina@gmail.com>
2022-02-10 19:54:09 +0100

    Hard arrays

--- World of Words/Hard.cs
+++ World of Words/Hard.cs
@@ -15,15 +15,60 @@ namespace World_of_Words
             Random wordIndex = new Random();
 
             string[] allWords = File.ReadAllLines(@"Words.txt");
+            string[,] viewWords = new string[3, 8];
+            string lineToPrint = string.Empty;
 
             for (int i = 0; i < 8; i++)
             {
                 int wordNumber = wordIndex.Next(0, allWords.Length);
                 string word = allWords[wordNumber];
 
-                Console.WriteLine(wordNumber + word);
-
+                lineToPrint += word + " ";
             }
+
+            viewWords[0, 0] = 1.ToString();
+            viewWords[0, 1] = 2.ToString();
+            viewWords[0, 2] = 3.ToString();
+            viewWords[0, 3] = 4.ToString();
+            viewWords[0, 4] = 5.ToString();
+            viewWords[0, 5] = 6.ToString();
+            viewWords[0, 6] = 7.ToString();
+            viewWords[0, 7] = 8.ToString();
+
+            viewWords[1, 0] = lineToPrint;
+            viewWords[1, 1] = lineToPrint;
+            viewWords[1, 2] = lineToPrint;
+            viewWords[1, 3] = lineToPrint;
+            viewWords[1, 4] = lineToPrint;
+            viewWords[1, 5] = lineToPrint;
+            viewWords[1, 6] = lineToPrint;
+            viewWords[1, 7] = lineToPrint;
+
+            viewWords[2, 0] = lineToPrint;
+            viewWords[2, 1] = lineToPrint;
+            viewWords[2, 2] = lineToPrint;
+            viewWords[2, 3] = lineToPrint;
+            viewWords[2, 4] = lineToPrint;
+            viewWords[2, 5] = lineToPrint;
+            viewWords[2, 6] = lineToPrint;
+            viewWords[2, 7] = lineToPrint;
+
+
+            static void displayArray(string[,] viewWords)
+            {
+                for (int k = 0; k < viewWords.GetLength(0); k++)
+                {
+                    for (int l = 0; l < viewWords.GetLength(1); l++)
+                    {
+                        Console.Write(viewWords[k, l]);
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+            displayArray(viewWords);
         }
     }
 }
